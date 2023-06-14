f4eb81e9bdc3364bdac9069ba9f96179597f46ba
Arpit <arpittyagirocks@gmail.com>
2023-06-14 13:19:18 +0530

    new next app

---
+++ app/numbers/one.js
@@ -0,0 +1,7 @@
+export default function one(){
+    return(
+        <>
+            <div>The page number one</div>
+        </>
+    )
+}
---
+++ pages/hello.js
@@ -0,0 +1,7 @@
+export default function one(){
+    return(
+        <>
+            <div>This is the hello page</div>
+        </>
+    )
+}
