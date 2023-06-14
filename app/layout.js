60d345c863fe0ede308dfc3de57089a1653ee648
Arpit <arpittyagirocks@gmail.com>
2023-06-14 14:40:54 +0530

    Understanding app routing in Next.js

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="sticky top-0 w-screen h-16 bg-neutral-900 text-center text-2xl ">
+          This is the navbar from layout.js
+        </div>
+        {children}
+        </body>
     </html>
   )
 }
